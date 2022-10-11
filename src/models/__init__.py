26e2955d88c143260191c8e0861c99a631914a03
Dieze <shahine.bouabid@lmh.ox.ac.uk>
2022-10-11 10:27:07 +0100

    added SVGP

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,6 @@
 from .exact_gp import ExactGP
 from .rff_gp import RFFGP
+from .svgp import SVGP
 from .thermalboxes_gp import ThermalBoxesGP
 
-__all__ = ['ExactGP', 'RFFGP', 'ThermalBoxesGP']
+__all__ = ['ExactGP', 'RFFGP', 'SVGP', 'ThermalBoxesGP']
