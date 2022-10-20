f3629d465d2a6f6c67541f723bf3d359139b4a7e
Dieze <shahine.bouabid@lmh.ox.ac.uk>
2022-10-20 20:35:24 +0100

    functional spatial GP notebook

--- src/preprocessing/preprocess_data.py
+++ src/preprocessing/preprocess_data.py
@@ -26,7 +26,7 @@ def get_fair_params():
 
 
 def load_emissions_dataset(filepath):
-    inputs = xr.open_dataset(filepath).compute()
+    inputs = xr.open_dataset(filepath).compute().isel(latitude=slice(0, 40), longitude=slice(0, 40))
     inputs.CO2.data = inputs.CO2.data / GtC_to_GtCO2
     inputs.CO2.attrs['units'] = 'GtC'
     inputs.CH4.data = inputs.CH4.data * Gt_to_Mt
@@ -39,7 +39,7 @@ def load_emissions_dataset(filepath):
 
 
 def load_response_dataset(filepath):
-    outputs = xr.open_dataset(filepath).compute()
+    outputs = xr.open_dataset(filepath).compute().isel(lat=slice(0, 40), lon=slice(0, 40))
     return outputs
 
 
