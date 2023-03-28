fixed cross-covariance thing

src/evaluation/visualisation.py
@@ -86,15 +86,17 @@ def plot_contourf_with_zonal_avg(field, title="", levels=20, fontsize=14):
     return fig, (mapax, plotax, barax), cx
 
 
-def plot_contourf_on_ax(field, fig, ax, levels=20, cmap='RdBu_r', vmax=None, title="", fontsize=14, colorbar=False):
+def plot_contourf_on_ax(field, fig, ax, levels=20, cmap='RdBu_r', vmax=None, vmin=None, title="", fontsize=14, colorbar=False):
     '''input field should be a 2D xarray.DataArray on a lat/lon grid.
     '''
     wrap_data, wrap_lon = add_cyclic_point(field.values,
                                            coord=field.lon,
                                            axis=field.dims.index('lon'))
-    if not vmax:
+    if vmax is None:
         vmax = np.abs(wrap_data).max()
-    levels = np.linspace(-vmax.round(), vmax.round(), levels)
+    if vmin is None:
+        vmin = -vmax
+    levels = np.linspace(vmin, vmax, levels)
     cx = ax.contourf(wrap_lon,
                      field.lat,
                      wrap_data,

src/models/thermalboxes_gp.py
@@ -46,22 +46,21 @@ class ThermalBoxesGP(GP):
         return means
 
     def _compute_covariance(self, scenario_dataset):
-        I = compute_I(scenario_dataset, self.kernel, self.d)
+        I = compute_I(scenario_dataset, self.kernel, self.q, self.d)
         Kj = compute_covariance(scenario_dataset, I, self.q, self.d)
+        Kj = gpytorch.add_jitter(Kj)
         return Kj
 
     def train_prior_dist(self):
-        Kj = self._compute_covariance(self.train_scenarios)
         train_mean = torch.zeros_like(self.train_scenarios.tas)
-        train_covar = gpytorch.add_jitter(Kj.sum(dim=-1))
+        train_covar = self._compute_covariance(self.train_scenarios)
         train_prior_dist = distributions.MultivariateNormal(train_mean, train_covar)
         train_prior_dist = self.likelihood(train_prior_dist)
         return train_prior_dist
 
     def forward(self, scenario_dataset):
         mean = torch.zeros_like(scenario_dataset.tas)
-        Kj = self._compute_covariance(scenario_dataset)
-        covar = gpytorch.add_jitter(Kj.sum(dim=-1))
+        covar = self._compute_covariance(scenario_dataset)
         prior_dist = distributions.MultivariateNormal(mean, covar)
         return prior_dist
 

src/models/utils.py
@@ -22,14 +22,14 @@ def compute_means(scenario_dataset):
     return means
 
 
-def compute_I(scenario_dataset, kernel, d):
-    I = [compute_I_scenario(scenario_dataset, scenario, kernel, d)
+def compute_I(scenario_dataset, kernel, q, d):
+    I = [compute_I_scenario(scenario_dataset, scenario, kernel, q, d)
          for scenario in scenario_dataset.scenarios.values()]
     I = torch.cat(I, dim=-2)
     return I
 
 
-def compute_I_scenario(scenario_dataset, scenario, kernel, d):
+def compute_I_scenario(scenario_dataset, scenario, kernel, q, d):
     mu, sigma = scenario_dataset.mu_inputs, scenario_dataset.sigma_inputs
     scenario_emissions_std = (scenario.full_inputs - mu) / sigma
     dataset_emissions_std = (scenario_dataset.full_inputs - mu) / sigma
@@ -39,16 +39,21 @@ def compute_I_scenario(scenario_dataset, scenario, kernel, d):
     for t in range(1, len(scenario_emissions_std)):
         I_old = I[:, t - 1]
         K_new = K[:, t]
-        I_new = step_I(I_old, K_new, d.unsqueeze(0))
+        I_new = step_I(I_old, K_new, q.unsqueeze(0), d.unsqueeze(0))
         I[:, t] = I_new.squeeze()
     return I
 
 
 def compute_covariance(scenario_dataset, I, q, d):
+    nboxes = len(q)
+    I = I[..., None].repeat(1, 1, 1, nboxes).view(I.size(0), I.size(1), -1)
+    d = d.repeat(nboxes)
+    q = q.repeat(nboxes)
     Kj = [compute_covariance_scenario(scenario_dataset, scenario, I, q, d)
           for scenario in scenario_dataset.scenarios.values()]
     Kj = torch.cat(Kj, dim=-2)
-    Kj = scenario_dataset.trim_hist(Kj)
+    Kj = scenario_dataset.trim_hist(Kj).sum(dim=-1)
+    Kj = 0.5 * (Kj + Kj.T)
     return Kj
 
 